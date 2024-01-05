9455cd734cf499ae65241b747620f703f8a35ace
ynd <yandepgy123@gmailcom>
2024-01-05 19:44:49 +0700

    HHHH

---
+++ app/src/main/java/com/example/banksampah2/HOME.kt
@@ -0,0 +1,11 @@
+package com.example.banksampah2
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class HOME : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_home)
+    }
+}
